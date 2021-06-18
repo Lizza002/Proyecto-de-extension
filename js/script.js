c8b307e53b99bd5a8c12bc4baaf8a108285a0f96
LAPTOP-ANKIVNK0\anali <analiarivalopez@gmail.com>
2021-06-18 19:56:37 -0400

    Interfaz del adm terminado

--- js/script.js
+++ js/script.js
@@ -3,16 +3,19 @@ const targets = document.querySelectorAll('[data-target]')
 const content = document.querySelectorAll('[data-content]')
 
 targets.forEach(target => {
-    target.addEventListener('click', () => {
-        content.forEach(c => {
-            c.classList.remove('active')
+        target.addEventListener('click', () => {
+            content.forEach(c => {
+                c.classList.remove('active')
+            })
+            const t = document.querySelector(target.dataset.target)
+            t.classList.add('active')
         })
-        const t = document.querySelector(target.dataset.target)
-        t.classList.add('active')
     })
-})
-
-//INGRESAR
+    // ALERTA DE REGISTRADO 
+document.getElementById('boton1').onclick = function() {
+        alert('Has registrado exitosamente');
+    }
+    //INGRESAR
 
 /*Roles
 1: Adm
